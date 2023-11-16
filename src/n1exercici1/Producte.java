faccacf8f0b7cb989ca953ed296d51142c7295b5
JoanCaballero <caballero.portoles.joan@gmail.com>
2023-11-16 17:17:17 +0100

    Initial commit n1ex1

--- src/n1exercici1/Producte.java
+++ src/n1exercici1/Producte.java
@@ -1,27 +1,14 @@
 package n1exercici1;
 
 public class Producte {
-    private String nom;
-    private float preu;
+    private final String nom;
+    private final float preu;
 
     public Producte(String nom, float preu){
         this.nom = nom;
         this.preu = preu;
     }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
     public float getPreu() {
         return preu;
-    }
-
-    public void setPreu(float preu) {
-        this.preu = preu;
     }
 }
