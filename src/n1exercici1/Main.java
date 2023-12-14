eb1e7a0a101a57211e160bbe55f59b5b6ddc0c85
JoanCaballero <caballero.portoles.joan@gmail.com>
2023-12-14 12:16:57 +0100

    Correcció n1

--- src/n1exercici1/Main.java
+++ src/n1exercici1/Main.java
@@ -18,18 +18,18 @@ public class Main {
 
             switch (opcio) {
                 case 1:
-                    afegirProducte(venda);
+                    venda.afegirProducte();
                     break;
                 case 2:
                     try{
-                        opcio = finalitzarCompra(venda);
+                        opcio = venda.finalitzarCompra();
                     }
                     catch(VendaBuidaException e){
                         System.out.println(e.getMessage());
                     }
                     break;
                 case 3:
-                    generarErrorArrayIndexOutOfBounds(venda);
+                    venda.generarErrorArrayIndexOutOfBounds();
                     break;
                 case 4:
                     break;
@@ -38,40 +38,5 @@ public class Main {
             }
         } while (opcio != 4);
         System.out.println("Fins Aviat!");
-    }
-
-    private static void afegirProducte(Venda venda) {
-        System.out.println("Introdueix el nom del producte que vols afegir a la compra: ");
-        String nom = sca.nextLine();
-        sca.nextLine();
-        boolean correcte = false;
-        float preu = 0;
-        do {
-            try {
-                System.out.println("Introdueix el preu del producte anterior:");
-                preu = sca.nextFloat();
-                correcte = true;
-            } catch (Exception e) {
-                System.out.println("Introdueix un valor vàlid.");
-                sca.nextLine();
-            }
-        } while (!correcte);
-        Producte producte = new Producte(nom, preu);
-        venda.afegirProducte(producte);
-    }
-
-    private static int finalitzarCompra(Venda venda) throws VendaBuidaException {
-        venda.calcularTotal();
-        System.out.println("El preu total de la compra es: " + venda.getPreuTotal());
-        return 4;
-    }
-
-    private static void generarErrorArrayIndexOutOfBounds(Venda venda) {
-        ArrayList<Producte> llista = venda.getProductes();
-        try {
-            Producte prod = llista.get(llista.size());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
--- src/n1exercici1/Venda.java
+++ src/n1exercici1/Venda.java
@@ -1,14 +1,17 @@
 package n1exercici1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Venda {
-    private final ArrayList<Producte> productes= new ArrayList<>();
+    private final ArrayList<Producte> productes;
     private float preuTotal;
     private int cont = 0;
+    static Scanner sca = new Scanner(System.in);
 
     public Venda(){
         this.preuTotal = 0;
+        productes = new ArrayList<>();
     }
 
     public ArrayList<Producte> getProductes() {
@@ -32,9 +35,37 @@ public class Venda {
                 throw new VendaBuidaException();
             }
     }
-
-    public void afegirProducte(Producte producte){
+    public void afegirProducte(){
+        System.out.println("Introdueix el nom del producte que vols afegir a la compra: ");
+        String nom = sca.nextLine();
+        sca.nextLine();
+        boolean correcte = false;
+        float preu = 0;
+        do {
+            try {
+                System.out.println("Introdueix el preu del producte anterior:");
+                preu = sca.nextFloat();
+                correcte = true;
+            } catch (Exception e) {
+                System.out.println("Introdueix un valor vàlid.");
+                sca.nextLine();
+            }
+        } while (!correcte);
+        Producte producte = new Producte(nom, preu);
         productes.add(producte);
+    }
+    public int finalitzarCompra() throws VendaBuidaException {
+        calcularTotal();
+        System.out.println("El preu total de la compra es: " + getPreuTotal());
+        return 4;
+    }
+    public void generarErrorArrayIndexOutOfBounds() {
+        ArrayList<Producte> llista = getProductes();
+        try {
+            Producte prod = llista.get(llista.size());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
